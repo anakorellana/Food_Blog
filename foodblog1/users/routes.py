613304949c8ddace0486cf531e6c3574fc35f3d1
Ana Karina Orellana <anakorellanasilva@gmail.com>
2020-09-15 18:31:33 +0000

    Added send reset email

--- foodblog1/users/routes.py
+++ foodblog1/users/routes.py
@@ -84,7 +84,8 @@ def reset_request():
     form = RequestResetForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        flash('An email has been sent with instructions to reset your password.', 'info')
+        send_reset_email(user)
+	flash('An email has been sent with instructions to reset your password.', 'info')
         return redirect(url_for('users.login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
